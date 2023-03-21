8a479a6406cb96b212ca85958720a97261e2e6aa
ark-232 <arkady232@gmail.com>
2023-03-21 10:07:58 -0600

    script

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+# Install Flask
+pip install flask
 
+# Kill any existing Python processes
+pkill python
+
+# Perform a git pull to update the code
+git pull
+
+# Run the app
 sudo python3 src/app.py
