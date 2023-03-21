882320ab52353f443a630ad64dd91b81c93920fc
B00TK1D <stearns.josiah@gmail.com>
2023-03-21 01:42:41 -0600

    Initial push - who knows, it might work

--- src/router.py
+++ src/router.py
@@ -8,7 +8,7 @@ app = flask.Flask(__name__)
 def run():
     global app
     # Run the app
-    app.run(port=8088, host="0.0.0.0", debug=True, use_evalex=False, exclude_patterns=["/app/exploits/*"])
+    app.run(port=2000, host="0.0.0.0", debug=True, use_evalex=False, exclude_patterns=["/app/exploits/*"])
 
 ### Routes ###
 @app.route("/", methods=["GET"])
